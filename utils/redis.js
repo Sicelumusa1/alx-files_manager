08352673145db7ca6a8a14c0b94ef5393fc9968c
Sicelumusa <musaqwabe@gmail.com>
2024-05-12 07:48:08 +0200

    Fixed eslint

--- utils/redis.js
+++ utils/redis.js
@@ -8,7 +8,7 @@ class RedisClient {
     // Handle errors
     this.client.on('error', (err) => {
       console.error('Redis error:', err);
-    })
+    });
   }
 
   async isAlive() {
@@ -29,8 +29,8 @@ class RedisClient {
         } else {
           resolve(value);
         }
-      })
-    }) 
+      });
+    })
   }
 
   async set(key, value, durationInSeconds) {
@@ -41,20 +41,20 @@ class RedisClient {
         } else {
           resolve();
         }
-      })
-    })
+      });
+    });
   }
 
   async del(key) {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       this.client.del(key, (err, deleteCount) => {
         if (err) {
           reject(err);
         } else {
           resolve(deleteCount);
         }
-      })
-    })
+      });
+    });
   }
 }
 
